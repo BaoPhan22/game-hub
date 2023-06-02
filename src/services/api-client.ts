af892357b6cc8c025fff40c21d2cd4ca7376a5cc
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-06-02 12:02:47 +0700

    display critic score

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: "4599edd7f0074f0a8f861b3da7e1b922"
-  }
+    key: "4599edd7f0074f0a8f861b3da7e1b922",
+  },
 });

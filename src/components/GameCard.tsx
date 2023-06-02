9b47a7ace1f0d65e7de4234b0b59049d4b5ae0ae
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2023-06-02 11:18:43 +0700

    Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 
@@ -8,13 +7,13 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-  <Card overflow='hidden' borderRadius={10}>
-    <Image src={game.background_image}/>
-    <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-    </CardBody>
-  </Card>
-    );
+    <Card overflow="hidden" borderRadius={10}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default GameCard;
